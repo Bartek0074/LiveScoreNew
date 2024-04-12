827fa8fab1e412f420fe26d02d24a0ab19bc7146
Bartek0074 <bartekdo123@gmail.com>
2024-04-12 23:29:58 +0200

    changed player rating background colors values

--- src/components/PlayerRating/PlayerRating.tsx
+++ src/components/PlayerRating/PlayerRating.tsx
@@ -11,11 +11,11 @@ export default function PlayerRating({ value, size = 'medium' }: Props) {
 		[styles.ratingSmall]: size === 'small',
 		[styles.ratingMedium]: size === 'medium',
 		[styles.ratingLarge]: size === 'large',
-		[styles.ratingVeryGood]: value >= 9.5,
-		[styles.ratingGood]: value < 9.5 && value >= 7.5,
-		[styles.ratingAverage]: value < 7.5 && value >= 6,
-		[styles.ratingBad]: value < 6 && value >= 3.5,
-		[styles.ratingVeryBad]: value < 3.5,
+		[styles.ratingVeryGood]: value >= 9.0,
+		[styles.ratingGood]: value < 9.0 && value >= 7.5,
+		[styles.ratingAverage]: value < 7.5 && value >= 6.5,
+		[styles.ratingBad]: value < 6.5 && value >= 4.5,
+		[styles.ratingVeryBad]: value < 4.5,
 	});
 	return (
 		<div className={ratingClasses}>
